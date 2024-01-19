dff57053d323c1f2d5926e7c78c2bf7f20a36feb
Matheus Canovas <matheuscanovas9@gmail.com>
2024-01-19 01:33:30 -0300

    Prettiering the code

--- src/Pages/Pomodoro/index.jsx
+++ src/Pages/Pomodoro/index.jsx
@@ -1,28 +1,27 @@
 import styled from "styled-components";
-import Title from "../../components/Title"
+import Title from "../../components/Title";
 import ButtonRest from "../../components/ButtonRest";
 
 const Container = styled.div`
-    padding: 1rem;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-`
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
 
 const ButtonContainer = styled.section`
   display: flex;
   align-content: center;
   justify-content: space-around;
   width: 100%;
-`
+`;
 
 export default function PomodoroPage() {
   return (
     <Container>
       <Title>POMOCODE</Title>
-      <div>
-      </div>
+      <div></div>
       <ButtonContainer>
         <ButtonRest>Long Rest</ButtonRest>
         <ButtonRest>Short Rest</ButtonRest>
--- src/components/ButtonRest/index.jsx
+++ src/components/ButtonRest/index.jsx
@@ -1,25 +1,21 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 const UnselectedButton = styled.button`
-    background-color: transparent;
-    padding: 1rem 2rem;
-    color: #81E6D9;
-    font-family: 'GeistUltraLight';
-    border: 1px solid #81E6D9;
-    border-radius: 1rem;
-    transition: .5s;
-    &:hover {
-        background-color: #81E6D9;
-        color: black;
-        font-weight: bold;
-        letter-spacing: .1rem;
-    }
-`
+  background-color: transparent;
+  padding: 1rem 2rem;
+  color: #81e6d9;
+  font-family: "GeistUltraLight";
+  border: 1px solid #81e6d9;
+  border-radius: 1rem;
+  transition: 0.5s;
+  &:hover {
+    background-color: #81e6d9;
+    color: black;
+    font-weight: bold;
+    letter-spacing: 0.1rem;
+  }
+`;
 
-export default function ButtonRest({ children }) {
-    return (
-        <UnselectedButton>
-            {children}
-        </UnselectedButton>
-    )
+export default function ButtonRest({ children, isSelected }) {
+  return <UnselectedButton>{children}</UnselectedButton>;
 }
