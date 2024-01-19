add39ba2c4a18e7b33c269b29275243565e06199
Matheus Canovas <matheuscanovas9@gmail.com>
2024-01-19 19:59:14 -0300

    Transforming the appliction in tray application

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,24 +1,33 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu};
-
+use tauri::{CustomMenuItem, generate_context, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, Window};
+use tauri_plugin_positioner::{Position, WindowExt};
 
 
 fn main() {
     let quit = CustomMenuItem::new("quit".to_string(), "Quit");
     let tray_menu = SystemTrayMenu::new().add_item(quit);
     tauri::Builder::default()
+        .plugin(tauri_plugin_positioner::init())
         .system_tray(SystemTray::new().with_menu(tray_menu).with_id("PomoCode"))
-        .on_system_tray_event(|_app, event| match event {
+        .on_system_tray_event(|app, event| match event {
             SystemTrayEvent::LeftClick {
                 position: _,
                 size: _,
                 ..
             } => {
-            //     @TODO Implement this later
+                let window: Window = app.get_window("main").unwrap();
+                // use TrayCenter as initial window position
+                let _ = window.move_window(Position::TopRight);
+                if window.is_visible().unwrap() {
+                    window.hide().unwrap();
+                } else {
+                    window.show().unwrap();
+                    window.set_focus().unwrap();
+                }
             },
             _ => {}
         })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .run(generate_context!())
+        .expect("error while running tauri application")
 }
